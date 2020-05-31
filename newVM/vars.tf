6fe2399756488c83bdf8d49b7f2afebd0d388a18
vibiabraham81 <66189131+vibiabraham81@users.noreply.github.com>
2020-05-31 15:50:54 -0400

    new baseline

---
+++ newVM/vars.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+        default = "187-aa0350-deploying-an-azure-vm-with-terraform-nm"
+}
+
+variable "azurerm_network_interface" {
+        #default = "new-vNic"
+}
+
+variable "azurerm_storage_account" {
+        #default = "mydiagvibi2020"
+}
+
+variable "azurerm_virtual_machine" {
+        #default = "my-VM"
+}
