bdccfc7fbdae3803107ae1c26ea88c51a87d8859
vibiabraham81 <vibi.abraham@gmail.com>
2020-05-31 04:39:04 +0000

    updated resource name and storage name

--- AzureStorage/createstorage.tf
+++ AzureStorage/createstorage.tf
@@ -2,8 +2,8 @@ provider "azurerm" {
     version = 1.38
     }
 resource "azurerm_storage_account" "lab" {
-  name                     = "Enter Storage Account Name"
-  resource_group_name      = "Enter Resource Group"
+  name                     = "vibistorage5312020"
+  resource_group_name      = "156-820806-deploy-an-azure-storage-account-with-te"
   location                 = "East US"
   account_tier            = "Standard"
   account_replication_type = "LRS"
